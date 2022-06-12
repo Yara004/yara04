c16e27d1c7e7095d81663a0ec4f5b4f270f0c3b4
Yara.mrhb <97759301+Yara004@users.noreply.github.com>
2022-06-12 12:35:55 +0300

    Initial commit

---
+++ batta5.php
@@ -0,0 +1,9 @@
+<?php
+echo ' 
+<!DOCTYPE html>
+<html>
+<img src="assets/img/8023ca21-e622-4ce2-bead-84def4e1b5d6.jpg" width="100%" height="100%"/>
+<a href="index.php">Click here</a> to sign out.
+</html>
+';
+?>
